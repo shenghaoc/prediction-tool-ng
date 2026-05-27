5f2eb0b8694e47aa0ac66fe3b9b7c245e4b905e5
shenghaoc <shenghaoc@outlook.com>
2026-05-27 18:18:35 +0800

    Upgrade ng2-charts from v5 to v10 and migrate to new standalone API (#104)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,30 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideCharts } from 'ng2-charts';
+import {
+  CategoryScale,
+  Filler,
+  LinearScale,
+  LineController,
+  LineElement,
+  PointElement,
+  Tooltip
+} from 'chart.js';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideClientHydration(),
+    provideCharts({
+      registerables: [
+        LineController,
+        LinearScale,
+        CategoryScale,
+        PointElement,
+        LineElement,
+        Filler,
+        Tooltip,
+      ]
+    }),
   ]
 };
